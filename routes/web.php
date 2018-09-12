b9e5a112fbb7e2d589e979d4039ca1b99200ccc3
boolfalse <boolfalse@mail.ru>
2018-09-12 16:18:05 +0300

    02 - Laravel and Redis

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Redis;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +14,6 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+    return Cache::get('foo');
 });
