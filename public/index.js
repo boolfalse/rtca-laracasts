2104b4b88b004496bc40b9ed36e8a30135403650
boolfalse <boolfalse@mail.ru>
2018-09-12 15:58:13 +0300

    https://laracasts.com/series/real-time-laravel-with-socket-io/episodes/1

---
+++ public/index.js
@@ -0,0 +1,25 @@
+
+var app = require('express')();
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    // console.log('A connection was made.');
+
+    socket.on('chat.message', function (message) {
+        // console.log(message);
+
+        io.emit('chat.message', message);
+    });
+
+    socket.on('disconnect', function () {
+        io.emit('chat.message', "User has Disconnected!");
+    });
+});
